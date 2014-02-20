8e0e0af77df5595288354e5869443c353b121bf2
cristian738 <cristhian_738@hotmail.com>
2014-02-20 11:47:07 -0700

    Ventana
    Inicio de una ventana

---
+++ Ejercicios/src/Mostrar.java
@@ -0,0 +1,16 @@
+
+public class Mostrar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		Ventana nueva = new  Ventana(0,0,null);
+		nueva.setaltura(200);
+		nueva.setancho(400);
+		nueva.setitulo("Ventana");
+		
+		nueva.mostrar();
+	}
+
+}
